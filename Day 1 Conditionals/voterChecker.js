70e67e67abeba18bf18a8cd6c95a8c838482d78b
Yash <yashterekar26@gmail.com>
2025-06-03 02:30:40 +0530

    voter checker task

---
+++ Day 1 Conditionals/voterChecker.js
@@ -0,0 +1,12 @@
+let nationality = "Indian";
+let age = 22;
+
+if (nationality.toLowerCase() == "indian") {
+  if (age >= 18) {
+    console.log("You can Vote");
+  } else {
+    console.log("You are minor");
+  }
+} else {
+  console.log("You are not Indian");
+}
