a77593da6385d8ba5b3fcae36605c40fb1b6b287
Yash <yashterekar26@gmail.com>
2025-06-03 02:12:21 +0530

    ageChecker added

---
+++ Day 1 Conditionals/ageChecker.js
@@ -0,0 +1,13 @@
+let age = 65;
+
+if (age <= 4) {
+  console.log("Baby");
+} else if (age <= 12) {
+  console.log("Child");
+} else if (age <= 18) {
+  console.log("Teen");
+} else if (age <= 60) {
+  console.log("Adult");
+} else {
+  console.log("Old");
+}
