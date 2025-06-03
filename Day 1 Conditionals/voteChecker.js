afacc7661c1156f2b1f5f5964319b713347eff3b
Yash <yashterekar26@gmail.com>
2025-06-03 13:51:10 +0530

    vote checker

--- Day 1 Conditionals/voteChecker.js
+++ Day 1 Conditionals/voteChecker.js
@@ -3,10 +3,10 @@ let age = 22;
 
 if (nationality.toLowerCase() == "indian") {
   if (age >= 18) {
-    console.log("You can Vote");
+    console.log("You can vote");
   } else {
     console.log("You are minor");
   }
 } else {
-  console.log("You are not Indian");
+  console.log("You are not indian");
 }
