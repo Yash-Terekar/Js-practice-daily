afaee13d8eb0174c2e4390c1091aec8c41baecab
Yash <yashterekar26@gmail.com>
2025-06-06 00:00:16 +0530

    groceryCart

---
+++ Day 3 Arrays & Objects/groceryCart.js
@@ -0,0 +1,13 @@
+let cart = [
+  { name: "milk", price: 50 },
+  { name: "bread", price: 30 },
+  { name: "eggs", price: 60 },
+];
+let total = 0;
+
+for (let i = 0; i < cart.length; i++) {
+  let itemName = cart[i].name.charAt(0).toUpperCase() + cart[i].name.slice(1);
+  console.log(`${itemName} - ₹${cart[i].price}`);
+  total += cart[i].price;
+}
+console.log(`Total : ${total}`);
