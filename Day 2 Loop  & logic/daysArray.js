099bd2d4739bcf5e74f9b767b4316986ecd28d9c
Yash <yashterekar26@gmail.com>
2025-06-05 12:59:38 +0530

    daysArray added

---
+++ Day 2 Loop  & logic/daysArray.js
@@ -0,0 +1,13 @@
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednusday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+for (let i = 0; i < days.length; i++) {
+  console.log(`Day ${i + 1} ${days[i]}`);
+}
