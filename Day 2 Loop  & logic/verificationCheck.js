3ee3da920a9619b64f2c83cc298db5bad138bcc0
Yash <yashterekar26@gmail.com>
2025-06-05 12:54:01 +0530

    verification checked

---
+++ Day 2 Loop  & logic/verificationCheck.js
@@ -0,0 +1,8 @@
+let emailVerified = true;
+let phoneVerified = true;
+
+if (emailVerified === true && phoneVerified === true) {
+  console.log("Welcome cheff");
+} else {
+  console.log("You are not Verified");
+}
