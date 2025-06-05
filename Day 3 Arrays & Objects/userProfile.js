e38ca41fe1b16341443f0eea0f58d38ad442521e
Yash <yashterekar26@gmail.com>
2025-06-05 23:33:13 +0530

    userProfile

---
+++ Day 3 Arrays & Objects/userProfile.js
@@ -0,0 +1,11 @@
+let user = {
+  name: "Yash",
+  age: 22,
+  email: "yash@email.com",
+  isAdmin: true,
+};
+
+console.log(`Name: ${user.name}`);
+console.log(`Age: ${user.age}`);
+console.log(`Email: ${user.email}`);
+console.log(`IsAdmin: ${user.isAdmin}`);
