c87b3932768df7afee12099ff727802e51fd5d6e
Yash <yashterekar26@gmail.com>
2025-06-05 13:05:18 +0530

    traffic signal script

---
+++ Day 2 Loop  & logic/trafficSignal.js
@@ -0,0 +1,12 @@
+let color = "Green";
+
+switch (color) {
+  case "Red":
+    console.log("Signal are Red Stop");
+    break;
+  case "Yellow":
+    console.log("Signal are Yellow go slow");
+    break;
+  case "Green":
+    console.log("Signal are Green Go");
+}
