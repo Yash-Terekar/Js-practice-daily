906f998403ab8bfd87b93d3fb0d1dff95402caac
Yash <yashterekar26@gmail.com>
2025-06-06 19:27:32 +0530

    Employee filter

--- Day 3 Arrays & Objects/employeeFilter.js
+++ Day 3 Arrays & Objects/employeeFilter.js
@@ -6,25 +6,37 @@ let employees = [
 ];
 
 for (let i = 0; i < employees.length; i++) {
-  let manager = employees[i].role.toLowerCase();
-  if (manager === "manager") {
-    console.log(`${employees[i].name} ${employees[i].role}`);
+  let employee = employees[i].role.toLowerCase();
+  if (employee === "manager") {
+    console.log(`${employees[i].name} = ${employees[i].role}`);
   }
 }
-
 for (let employee of employees) {
   if (employee.role.toLowerCase() === "manager") {
-    console.log(`${employee.name} ${employee.role}`);
+    console.log(`${employee.name} = ${employee.role}`);
   }
 }
 
-employees.forEach((employee) => {
-  if (employee.role.toLowerCase() === "manager") {
-    console.log(`${employee.name} ${employee.role}`);
-  }
-});
+// for (let i = 0; i < employees.length; i++) {
+//   let manager = employees[i].role.toLowerCase();
+//   if (manager === "manager") {
+//     console.log(`${employees[i].name} ${employees[i].role}`);
+//   }
+// }
 
-let managers = employees.filter(
-  (employee) => employee.role.toLowerCase() === "manager"
-);
-managers.forEach((manager) => console.log(`${manager.name} ${manager.role}`));
+// for (let employee of employees) {
+//   if (employee.role.toLowerCase() === "manager") {
+//     console.log(`${employee.name} ${employee.role}`);
+//   }
+// }
+
+// employees.forEach((employee) => {
+//   if (employee.role.toLowerCase() === "manager") {
+//     console.log(`${employee.name} ${employee.role}`);
+//   }
+// });
+
+// let managers = employees.filter(
+//   (employee) => employee.role.toLowerCase() === "manager"
+// );
+// managers.forEach((manager) => console.log(`${manager.name} ${manager.role}`));
