c6b855bae625732f21b50c961ed5fd1a89553aab
Yash <yashterekar26@gmail.com>
2025-06-06 19:32:52 +0530

    GitHub Profile

---
+++ Day 3 Arrays & Objects/githubProfile.js
@@ -0,0 +1,14 @@
+let devs = [
+  { name: "Yash", github: "https://github.com/Yash-Terekar" },
+  { name: "Ravi", github: "https://github.com/ravi" },
+  { name: "Aditya", github: "https://github.com/Aditya" },
+  { name: "Aniket", github: "https://github.com/Aniket" },
+  { name: "Shreyash", github: "https://github.com/Shreyash" },
+  { name: "Ravindra", github: "https://github.com/Ravindra" },
+  { name: "Nash", github: "https://github.com/Nash" },
+  { name: "Nakul", github: "https://github.com/Nakul" },
+];
+
+for (let i = 0; i < devs.length; i++) {
+  console.log(`${devs[i].name} - GitHub: ${devs[i].github}`);
+}
