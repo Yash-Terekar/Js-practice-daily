81fb79e9d9d1644806ab5ef12d13847c81570fd8
Yash <yashterekar26@gmail.com>
2025-06-07 16:23:52 +0530

    search person

--- Day 4 Logic Mix/searchPerson.js
+++ Day 4 Logic Mix/searchPerson.js
@@ -6,17 +6,14 @@ let employees = [
   { name: "Yash", dept: "Web", role: "Ai" },
   { name: "Ayush", dept: "HR", role: "Manager" },
 ];
-let search = "Dev";
+let search = "ayush";
+
 for (let employee of employees) {
-  if (search === employee.name) {
-    search = employee.name;
-    console.log(`${employee.name} ${employee.dept} ${employee.role}`);
-  } else if (search === employee.dept) {
-    search = employee.dept;
-    console.log(`${employee.name} ${employee.dept} ${employee.role}`);
-  }
-  if (search === employee.role) {
-    search = employee.role;
+  if (
+    search.toLowerCase() === employee.name.toLowerCase() ||
+    search.toLowerCase() === employee.dept.toLowerCase() ||
+    search.toLowerCase() === employee.role.toLowerCase()
+  ) {
     console.log(`${employee.name} ${employee.dept} ${employee.role}`);
   }
 }
