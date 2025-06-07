7134f20d80188df25f22bd7cb53dce9c29bd1271
Yash <yashterekar26@gmail.com>
2025-06-07 15:55:33 +0530

    cart discount

---
+++ Day 4 Logic Mix/cartDiscount.js
@@ -0,0 +1,19 @@
+let cart = [
+  { item: "Bag", price: 900 },
+  { item: "Shoes", price: 1200 },
+  { item: "Watch", price: 2500 },
+];
+let total = 0;
+for (let prices of cart) {
+  total += prices.price;
+}
+let discount = 0;
+if (total > 3000) {
+  discount = 0.3;
+} else if (total > 2000) {
+  discount = 0.2;
+}
+
+let finalPrice = total - total * discount;
+
+console.log(`Cart Total is: ${total} And Discounted Price is:${finalPrice}`);
