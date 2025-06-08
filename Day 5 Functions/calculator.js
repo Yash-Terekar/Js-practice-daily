e6590fffd67879d19a402ae602a84ef0b3ccc8c0
Yash <yashterekar26@gmail.com>
2025-06-09 01:16:25 +0530

    calculator

--- Day 5 Functions/calculator.js
+++ Day 5 Functions/calculator.js
@@ -1,20 +1,30 @@
-function calculator(num1, num2, opration) {
-  if (opration === "mul") {
-    return num1 * num2;
-  } else if (opration === "div") {
-    return num1 / num2;
-  } else if (opration === "add") {
-    return num1 + num2;
-  } else if (opration === "sub") {
-    return num1 - num2;
+function calculator(num1, num2, operation) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    console.log("Invalid input please provide valid numbers");
+  }
+  switch (operation) {
+    case "mul":
+      return num1 * num2;
+    case "div":
+      if (num1 === 0 || num2 === 0) {
+        console.log("Division not possible in 0 so provide valid inputs");
+      } else {
+        return num1 / num2;
+      }
+    case "add":
+      return num1 + num2;
+    case "sub":
+      return num1 - num2;
+    default:
+      console.log("Please provide valid numbers");
   }
 }
 
 let multi = calculator(12, 3, "mul");
 console.log(multi);
-let add = calculator(12, 3, "add");
-console.log(add);
-let div = calculator(12, 3, "div");
+let div = calculator(12, 0, "div");
 console.log(div);
-let sub = calculator(12, 3, "sub");
+let add = calculator(12, 4, "add");
+console.log(add);
+let sub = calculator(12, 4, "sub");
 console.log(sub);
