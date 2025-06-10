c28ef30417defa481a635388982a27038c13e676
Yash <yashterekar26@gmail.com>
2025-06-10 14:52:13 +0530

    setInterval setTimeout

---
+++ Day 5 Functions/app.js
@@ -0,0 +1,14 @@
+let count = 5;
+
+const interval = setInterval(() => {
+  console.log(count);
+  count--;
+
+  if (count < 0) {
+    clearInterval(interval);
+  }
+}, 1000);
+
+setTimeout(() => {
+  console.log(`Boom!`);
+}, 7000);
