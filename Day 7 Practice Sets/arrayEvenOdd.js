e9844d9333aef872533f08397f8bc22de40bfb86
Yash <yashterekar26@gmail.com>
2025-06-15 02:44:13 +0530

    even odd separator

---
+++ Day 7 Practice Sets/arrayEvenOdd.js
@@ -0,0 +1,31 @@
+const array = [2, 3, 8, 6, 75, 34, 43, 63, 46];
+
+function separateEvenOdd(number) {
+  if (number % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+evenOdd = (num) => (num % 2 == 0 ? "Even" : "Odd");
+
+function loops(arr) {
+  for (let num of arr) {
+    const isEven = evenOdd(num);
+    console.log(`Number ${num} is ${isEven}`);
+  }
+}
+// function isEven(number) {
+//   if (typeof number !== "number" || isNaN(number)) {
+//     throw new Error("Input must be a number");
+//   }
+//   return number % 2 === 0;
+// }
+loops(array);
+// function logEvenOdd(numbers) {
+//   for (let num of numbers) {
+//     console.log(`Number ${num} is ${isEven(num) ? "Even" : "Odd"}`);
+//   }
+// }
+// logEvenOdd(array);
