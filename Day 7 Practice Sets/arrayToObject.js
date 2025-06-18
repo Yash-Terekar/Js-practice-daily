b6fec7aaaf12e085218c358288fc37bc9c5e399a
Yash <yashterekar26@gmail.com>
2025-06-18 23:11:28 +0530

    Array to Object

---
+++ Day 7 Practice Sets/arrayToObject.js
@@ -0,0 +1,14 @@
+// 4. Convert array of objects to an object (use reduce)
+
+const arr = [
+  { id: 1, name: "A" },
+  { id: 2, name: "B" },
+  { id: 3, name: "C" },
+  { id: 4, name: "D" },
+];
+
+const obj = arr.reduce((p, c) => {
+  p[c.id] = c.name;
+  return p;
+}, {});
+console.log(obj);
