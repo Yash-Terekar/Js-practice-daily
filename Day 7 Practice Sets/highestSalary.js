970d00f68fa82da87ad4b6ba2f0514cd1a24ec61
Yash <yashterekar26@gmail.com>
2025-06-18 23:22:51 +0530

    Highest Salary

---
+++ Day 7 Practice Sets/highestSalary.js
@@ -0,0 +1,10 @@
+// 5. Find user with highest salary
+const employee = [
+  { name: "Aniket", salary: 31000 },
+  { name: "Yash", salary: 46000 },
+  { name: "Aditya", salary: 26000 },
+  { name: "Siddhu", salary: 36000 },
+];
+
+const highestSalary = employee.reduce((p, c) => (p.salary > c.salary ? p : c));
+console.log(highestSalary);
