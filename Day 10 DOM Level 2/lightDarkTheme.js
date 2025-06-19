4ae35b69ad0cc7a867c1c703a3e25d87bfde93f1
Yash <yashterekar26@gmail.com>
2025-06-20 00:00:35 +0530

    Toggle Dark/Light Mode

---
+++ Day 10 DOM Level 2/lightDarkTheme.js
@@ -0,0 +1,17 @@
+// 7. Toggle Light/Dark Theme
+// A toggle switch or button
+// When clicked, it adds/removes a dark-mode class to <body>
+
+const button = document.querySelector("button");
+const body = document.body;
+const lightMode = "Light Mode";
+
+button.addEventListener("click", () => {
+  body.classList.toggle("dark-theme");
+  button.classList.toggle("light-theme");
+  if (button.textContent === "Dark Mode") {
+    button.textContent = lightMode;
+  } else {
+    button.textContent = "Dark Mode";
+  }
+});
