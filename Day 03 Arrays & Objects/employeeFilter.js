34ffdbc8653273ecb50b07cf5b11e0eaff5bc7e7
Yash <yashterekar26@gmail.com>
2025-06-20 01:10:24 +0530

    Employee Filter

--- Day 03 Arrays & Objects/employeeFilter.js
+++ Day 03 Arrays & Objects/employeeFilter.js
@@ -16,27 +16,3 @@ for (let employee of employees) {
     console.log(`${employee.name} = ${employee.role}`);
   }
 }
-
-// for (let i = 0; i < employees.length; i++) {
-//   let manager = employees[i].role.toLowerCase();
-//   if (manager === "manager") {
-//     console.log(`${employees[i].name} ${employees[i].role}`);
-//   }
-// }
-
-// for (let employee of employees) {
-//   if (employee.role.toLowerCase() === "manager") {
-//     console.log(`${employee.name} ${employee.role}`);
-//   }
-// }
-
-// employees.forEach((employee) => {
-//   if (employee.role.toLowerCase() === "manager") {
-//     console.log(`${employee.name} ${employee.role}`);
-//   }
-// });
-
-// let managers = employees.filter(
-//   (employee) => employee.role.toLowerCase() === "manager"
-// );
-// managers.forEach((manager) => console.log(`${manager.name} ${manager.role}`));
