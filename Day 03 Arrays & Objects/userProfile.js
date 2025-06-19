267a4e5c1bfeeef1074c2fa94690b771b4aa580c
Yash <yashterekar26@gmail.com>
2025-06-20 01:13:40 +0530

    User Profile Checker

--- Day 03 Arrays & Objects/userProfile.js
+++ Day 03 Arrays & Objects/userProfile.js
@@ -3,9 +3,11 @@ let user = {
   age: 22,
   email: "yash@email.com",
   isAdmin: true,
+  work: "Programmer",
 };
 
 console.log(`Name: ${user.name}`);
 console.log(`Age: ${user.age}`);
 console.log(`Email: ${user.email}`);
 console.log(`IsAdmin: ${user.isAdmin}`);
+console.log(`Work: ${user.work}`);
