30c5b00e7995373969226f22be15265e0d02f9e3
Yash <yashterekar26@gmail.com>
2025-06-19 20:29:57 +0530

    Input char counter

---
+++ Day 9 DOM Practice/charCounter.js
@@ -0,0 +1,12 @@
+// 5. Input Character Counter
+// A textarea + live character counter
+// As the user types, show the number of characters typed
+
+const input = document.querySelector("input");
+
+input.addEventListener("keypress", () => {
+  const h2 = document.querySelector("h2");
+  const text = input.value;
+  const count = text.length + 1;
+  h2.textContent = count;
+});
