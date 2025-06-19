768d082b9e444fc05699c040d83257cca00a7f2c
Yash <yashterekar26@gmail.com>
2025-06-19 17:23:58 +0530

    DOM Practice

---
+++ Day 9 DOM Practice/clickMe.js
@@ -0,0 +1,6 @@
+// 1. Click Counter Button
+// Create a button: “Click Me”
+// On each click, show how many times it has been clicked.
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => console.log("Clicked"));
