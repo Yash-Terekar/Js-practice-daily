f6e9e77d575099d01d0df788bbdc59d044348521
Yash <yashterekar26@gmail.com>
2025-06-21 01:50:03 +0530

    Voter Eligibility Checker

--- Day 04 Logics Mix/votingEligibility.js
+++ Day 04 Logics Mix/votingEligibility.js
@@ -1,7 +1,7 @@
 let people = [
   { name: "Rahul", age: 17, nationality: "Indian" },
   { name: "Sneha", age: 22, nationality: "Indian" },
-  { name: "Ali", age: 20, nationality: "Pakistani" },
+  { name: "John", age: 20, nationality: "France" },
   { name: "Yash", age: 22, nationality: "Indian" },
   { name: "Disha", age: 22, nationality: "Indian" },
   { name: "Sunil", age: 43, nationality: "Shri Lanka" },
