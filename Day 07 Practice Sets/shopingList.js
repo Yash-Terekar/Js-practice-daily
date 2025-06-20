628e3c25e08eccf3078b201cb37b2b971816f9e4
Yash <yashterekar26@gmail.com>
2025-06-21 02:12:55 +0530

    Shopping List Formatter

--- Day 07 Practice Sets/shopingList.js
+++ Day 07 Practice Sets/shopingList.js
@@ -4,6 +4,7 @@ const cart = [
   { item: "Milk", qty: 2 },
   { item: "Bread", qty: 1 },
   { item: "Tea", qty: 3 },
+  { item: "Biscuit", qty: 2 },
 ];
 
 const short = cart.map((i) => console.log(`${i.qty}x ${i.item}`));
