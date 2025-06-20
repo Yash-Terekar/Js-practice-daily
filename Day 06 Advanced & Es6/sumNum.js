131d78795fad5d22d0e6025d26a42eef319afe5b
Yash <yashterekar26@gmail.com>
2025-06-21 02:02:20 +0530

    Array Sum using Spread Operator

--- Day 06 Advanced & Es6/sumNum.js
+++ Day 06 Advanced & Es6/sumNum.js
@@ -2,7 +2,7 @@ function sumNum(...num) {
   return num.reduce((a, b) => a + b, 0);
 }
 
-const prices = [120, 300, 450];
+const prices = [120, 150, 450];
 
 const total = sumNum(...prices);
 console.log(`Total: ${total}`);
