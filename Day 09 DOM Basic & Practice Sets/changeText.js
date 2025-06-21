d22f458390a13aed662c5c7b7726b341deaa2542
Yash <yashterekar26@gmail.com>
2025-06-22 02:47:44 +0530

    Change Text using DOM

--- Day 09 DOM Basic & Practice Sets/changeText.js
+++ Day 09 DOM Basic & Practice Sets/changeText.js
@@ -1,6 +1,6 @@
 // 4. Change Text on Button Click
 // Add a paragraph with some text
-// Add a button that changes the paragraph text when clicked
+// Add a button that changes the paragraph text when clicked.
 
 const p = document.querySelector("p");
 const button = document.querySelector("button");
