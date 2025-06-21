f36c6dd54d6b651bbf494fe4aded65270ed323c9
Yash <yashterekar26@gmail.com>
2025-06-22 02:55:54 +0530

    Basic Color Changer

--- Day 10 DOM Level 2/Basics Projects/changeBG.js
+++ Day 10 DOM Level 2/Basics Projects/changeBG.js
@@ -1,6 +1,6 @@
 const button = document.querySelector("button");
 
-let colors = ["red", "blue", "green", "purple"];
+let colors = ["red", "blue", "green", "purple", "cyan"];
 let index = 0;
 button.addEventListener("click", () => {
   document.body.style.backgroundColor = colors[index];
