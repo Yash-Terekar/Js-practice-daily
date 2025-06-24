b20b267ed9779f8e4d28c24c361ba97eafce276e
Yash <yashterekar26@gmail.com>
2025-06-24 21:58:59 +0530

    Counter using Events

--- Day 10 DOM Level 2/Basics Projects/counter.js
+++ Day 10 DOM Level 2/Basics Projects/counter.js
@@ -1,8 +1,8 @@
 const sub = document.querySelector(".sub");
 const add = document.querySelector(".add");
 const h1 = document.querySelector("h1");
-
-let counter = parseInt(h1.textContent);
+let counter;
+counter = parseInt(h1.textContent);
 sub.addEventListener("click", () => {
   counter = counter - 1;
   h1.innerHTML = h1.textContent - 1;
