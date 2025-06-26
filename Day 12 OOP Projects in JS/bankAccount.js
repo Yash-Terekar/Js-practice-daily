3ae8b7e3271ab0497804bd2d8fa5326896f14bbd
Yash <yashterekar26@gmail.com>
2025-06-26 14:37:21 +0530

    Bank Transaction Handler

---
+++ Day 12 OOP Projects in JS/bankAccount.js
@@ -0,0 +1,32 @@
+// Day 12 - Project 1: Bank Account Handler System
+
+class BankUsers {
+  constructor(owner, balance = 0) {
+    (this.owner = owner), (this.balance = balance);
+  }
+
+  credit(amount) {
+    this.balance += amount;
+    return this.balance;
+  }
+
+  debit(amount) {
+    if (this.balance >= amount) this.balance -= amount;
+    else
+      console.log(`User ${this.owner} has Insufficent Balance ${this.balance}`);
+  }
+
+  printOutput() {
+    // console.log(`${this.owner} Credit: ${this.credit()}`);
+    console.log(`Hello ${this.owner} your remaining balance: ${this.balance}`);
+  }
+}
+
+const yash = new BankUsers("Yash", 26000);
+yash.credit(2000);
+yash.debit(4000);
+yash.printOutput();
+const aditya = new BankUsers("Aditya", 4000);
+aditya.credit(6000);
+aditya.debit(12000);
+// aditya.printOutput();
