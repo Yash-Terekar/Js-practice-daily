a372a66da73c7866e8c2e9d5160d2a6984426395
Yash <yashterekar26@gmail.com>
2025-06-28 19:22:51 +0530

    Callback Hell using setTimeout

---
+++ Day 13 Callbacks Promises/callbackHell.js
@@ -0,0 +1,16 @@
+setTimeout(() => {
+  console.log("Order Placed ➡️");
+  setTimeout(() => {
+    console.log("Preparing Food ➡️");
+    setTimeout(() => {
+      console.log("Packed ➡️");
+      setTimeout(() => {
+        console.log("Out for Delivery ➡️");
+        setTimeout(() => {
+          console.log("Delivered ➡️");
+          console.log("Thanks for ordering!");
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 2000);
